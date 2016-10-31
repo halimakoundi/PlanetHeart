add636d9e17813df56da578d54983e2e201583ff
Halima KOUNDI <hkoundi@gmail.com>
2016-10-31 16:15:01 +0000

    desparately trying things to display picture once taken

--- PlanetHeart/PlanetHeart.Droid/MainActivity.cs
+++ PlanetHeart/PlanetHeart.Droid/MainActivity.cs
@@ -1,12 +1,14 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.Net;
 using Android.OS;
 using Android.Provider;
 using Java.IO;
 using PlanetHeartPCL;
 using Xamarin.Forms.Platform.Android;
+using Environment = Android.OS.Environment;
+using Uri = Android.Net.Uri;
 
 namespace PlanetHeart.Droid
 {
@@ -15,10 +17,11 @@ namespace PlanetHeart.Droid
           Theme = "@style/AppTheme")]
     public class MainActivity : FormsAppCompatActivity
     {
-        private static readonly File File = new File(Environment.GetExternalStoragePublicDirectory(
+        private static  File _file = new File(Environment.GetExternalStoragePublicDirectory(
                                 Environment.DirectoryPictures), "tmp.jpg");
 
         private App _app;
+        private File _dir;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -29,11 +32,12 @@ namespace PlanetHeart.Droid
             Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
             _app = (Xamarin.Forms.Application.Current as App);
+            CreateDirectoryForPictures();
 
             _app.ShouldTakePicture += () =>
             {
                 var intent = new Intent(MediaStore.ActionImageCapture);
-                intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(File));
+                intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(_file));
                 StartActivityForResult(intent, 0);
             };
         }
@@ -41,13 +45,23 @@ namespace PlanetHeart.Droid
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
             var mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
-            var contentUri = Uri.FromFile(File);
+            var contentUri = Uri.FromFile(_file);
             mediaScanIntent.SetData(contentUri);
             SendBroadcast(mediaScanIntent);
-
-           // _app.ShowPicture(File.Path);
-            _app.ShowPicture("ic_photo.png");
+            _file = new File(_dir, $"myPhoto_{Guid.NewGuid()}.jpg");
+            _app.ShowPicture(_file.AbsolutePath);
+            //_app.ShowPicture("ic_photo.png");
             base.OnActivityResult(requestCode, resultCode, data);
+        }
+        private void CreateDirectoryForPictures()
+        {
+            _dir = new File(
+                Environment.GetExternalStoragePublicDirectory(
+                    Environment.DirectoryPictures), "CameraAppDemo");
+            if (!_dir.Exists())
+            {
+                _dir.Mkdirs();
+            }
         }
     }
 }
--- PlanetHeart/PlanetHeartPCL/Pages/AddItemPage.xaml.cs
+++ PlanetHeart/PlanetHeartPCL/Pages/AddItemPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace PlanetHeartPCL.Pages
 {
@@ -11,21 +12,35 @@ namespace PlanetHeartPCL.Pages
         {
             InitializeComponent();
             _app.ShouldShowPicture += ShowImage;
+            ItemPicture.GestureRecognizers.Add(new TapGestureRecognizer());
         }
 
         protected override void OnAppearing()
         {
             if (!_pictureHasBeenAdded)
             {
-                _app.TakePicture();
+                OpenCameraForPicture();
             }
             base.OnAppearing();
+        }
+
+        private void TakePicture(object sender, EventArgs eventArgs)
+        {
+            OpenCameraForPicture();
+        }
+
+        private void OpenCameraForPicture()
+        {
+            _app.TakePicture();
         }
 
         public void ShowImage(string filepath)
         {
             _pictureHasBeenAdded = true;
             ItemPicture.Source = ImageSource.FromFile(filepath);
+            ItemPicture.BackgroundColor = Color.Aqua;
+            ItemPicture.ScaleTo(2,250,null);
+
         }
     }
 }
